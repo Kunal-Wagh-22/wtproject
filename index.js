f76e9749c0f2c388a6391901b8cb3905f80a3265
kunal-tiger <Waghkunal314@gmail.com>
2022-09-21 14:04:05 +0530

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+const box =  
+
+document.getElementById("demo").addEventListener("click", myFunction)
+
+function myFunction(){
+    document.getElementById("yoo").style.bottom = "100px";
+    document.getElementById("yoo").style.transition = "all 1s ease";
+}
